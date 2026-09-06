f54eb1029112beff97377f89da8b9aafb3ddbff6
Joanna K <144917448+keisaj9006@users.noreply.github.com>
2026-09-06 14:31:38 +0100

    fix: close adaptive Daily Insight padding

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1109,6 +1109,7 @@ class _DailyInsightCard extends StatelessWidget {
                     ),
                   ],
                 ),
+              ),
               ],
             ),
           ),
